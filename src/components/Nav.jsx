fa981104c0b27a234fb8feac32673d90cb627d43
Batubo Victory <victorybatubo76@gmail.com>
2025-08-19 15:54:24 +0100

    Add: Create nav content and style it

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,24 @@
 function Nav(){
   return(
-    <nav>
+    <nav className='flex justify-between items-center'>
+      <h3 className='text-xl font-bold'>DevSnack</h3>
 
+      <ul>
+        <li>Home</li>
+      </ul>
+      <ul>
+        <li>Search</li>
+      </ul>
+      <ul>
+        <li>Favorites</li>
+      </ul>
+
+      <div className='bg-[#14B8A6] text-white py-1 px-1 rounded-full text-xl flex justify-start'>
+        {/* toggle div */}
+      <button className="mr-10 rotate-90">
+        🌙
+      </button>
+      </div>
     </nav>
   )
 }
