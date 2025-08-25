e475ec30e0be1ae9c155941fec7c3381b5189919
Batubo Victory <victorybatubo76@gmail.com>
2025-08-25 13:48:06 +0100

    Import Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import './icons/fontawesome';
 import Nav from './components/Nav';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <>
       <Nav />
+      <Hero />
     </>
   );
 }
