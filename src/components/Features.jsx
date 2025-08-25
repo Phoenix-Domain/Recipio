0166accff443cf49652e64f1d3151909993471ca
Batubo Victory <victorybatubo76@gmail.com>
2025-08-25 16:19:42 +0100

    Map meal and render card component

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect} from 'react'
 import axios from 'axios'
+import Card from './Card';
 
 function Features(){
   const [meal, setMeal] = useState([]);
@@ -12,6 +13,7 @@ function Features(){
       const response = await Promise.all(requests);
 
       const mealData =  response.map(res => res.data.meals[0])
+
       setMeal(mealData);
     }catch(err){
       console.log(err)
@@ -27,8 +29,10 @@ function Features(){
   return (
     <section>
       <h2>Featured recipes</h2>
-      <article className=''>
-
+      <article className='grid grid-cols-2 gap-5 p-4'>
+        {meal.map((meal, index) =>{
+          return <Card key={index} name={meal.strMeal} image={meal.strMealThumb} category={meal.strCategory} region={meal.strArea}/>
+        })}
       </article>
     </section>
   )
