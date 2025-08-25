0b9e9a7b7264f89a3015cb35560de98a3d5ce22f
Batubo Victory <victorybatubo76@gmail.com>
2025-08-25 16:34:44 +0100

    Style image div

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,27 @@
 function Card({ name, image, category, region}){
   return(
     <article className='flex flex-col gap-2'>
-      <div className="relative border">
+      <div className="relative">
         <div className="flex flex-col relative">
-          <div className="text-white flex justify-between py-2 px-1 absolute top-0 right-0 z-3">
-            <p className="text-4xl bg-white rounded-full px-2 font-bold text-gray-700">♡</p>
-          </div>
-            <div className="bg-[rgba(0,0,0,0.9)] text-white flex justify-between py-2 px-1 absolute bottom-0 left-0 z-3 w-full ">
-              <p className="bg-gray-500 p-1 rounded-full">{category}</p>
+          <div className="w-full h-[100%] absolute top-0 left-0 z-2 bg-[rgba(0,0,0,0.6)]">
 
-              <p className="bg-gray-500 p-1 rounded-full">{region}</p>
-            </div>
+          </div>
+          <div className="text-white flex justify-between py-2 px-1 absolute top-0 right-0 z-3">
+            <p className="text-4xl bg-[rgba(255,255,255,0.9)] rounded-full px-2 font-bold text-gray-700">♡</p>
+          </div>
+
+          <div className="bg-[rgba(0,0,0,0.9)] text-white flex justify-between py-2 px-1 absolute bottom-0 left-0 z-3 w-full ">
+            <p className="bg-gray-800 p-1 rounded-full">{category}</p>
+
+            <p className="bg-gray-800 p-1 rounded-full">{region}</p>
+          </div>
         <img src={image} alt={`Photo of ${name}`} className="rounded-md"/>
 
         
         </div>
       </div>
 
-      <div>
+      <div className="border">
         <h3>{name}</h3>
       </div>
       
