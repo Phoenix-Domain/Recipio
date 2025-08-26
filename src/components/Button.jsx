d6c3a20c94f9569f6d643d529b80e5710017d5fe
Batubo Victory <victorybatubo76@gmail.com>
2025-08-26 12:00:09 +0100

    Edit: Pass onclick prop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
-function Button({ content }){
+function Button({ content, onClick }){
   return(
-    <button className="text-white bg-[#07534a] py-2 px-4 rounded-md">
+    <button className="text-white bg-[#07534a] py-2 px-4 rounded-md" onClick={onClick}>
       {content}
     </button>
   )
