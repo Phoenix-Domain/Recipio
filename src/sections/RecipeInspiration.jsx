b188bbc10e1789ab8a933340eeabd241b21a9998
Batubo Victory <victorybatubo76@gmail.com>
2025-08-26 11:38:19 +0100

    Edit: Import button component

--- src/sections/RecipeInspiration.jsx
+++ src/sections/RecipeInspiration.jsx
@@ -1,7 +1,22 @@
+import Button from "../components/button"
+
 function RecipeInspiration(){
   return(
-    <section>
-      
+    <section className='p-1'>
+      <div className='flex justify-between my-4 items-center'>
+        <h2 className='text-3xl font-bold'>
+          Recipe Inspiration
+        </h2>
+        
+        <a href="" className='text-cyan-800 font-semibold'>
+          <Button content='Next Recipe' />
+        </a>
+      </div>
+
+      <article className='grid grid-cols-2 gap-5 p-4'>
+        
+      </article>
+
     </section>
   )
 }
